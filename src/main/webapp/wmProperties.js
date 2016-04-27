1e4721bbc1ac733b60c0c4a901cbbbe0321706ec
root <root@7c085df60b94>
2016-04-27 05:22:38 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,9 @@
+var _WM_APP_PROPERTIES = {
+  "defaultLanguage" : "en",
+  "displayName" : "SharedTestProject",
+  "homePage" : "Main",
+  "name" : "sharedtestproject",
+  "platformType" : "WEB",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
